6a0483f159b09dbff6337fab0998310a32b08fb9
Brspontes <brian.robert16@hotmail.com>
2021-04-01 21:17:26 -0300

    finishing modal adding

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from "react";
+import { Component, createRef, FormEvent, useState } from "react";
 import { FiCheckSquare } from "react-icons/fi";
 
 import { Form } from "./styles";
@@ -17,8 +17,18 @@ export function ModalAddFood({
   handleAddFood,
 }: modalAddProps) {
 
+  const [image, setImage] = useState('')
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [description, setDescription] = useState('')
+
   async function handleSubmit() {
-        //handleAddFood(data);
+    handleAddFood({
+      image,
+      name,
+      price,
+      description
+    });
     onRequestClose();
   }
 
@@ -26,12 +36,12 @@ export function ModalAddFood({
     <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
       <Form onSubmit={handleSubmit}>
         <h1>Novo Prato</h1>
-        <Input name="image" placeholder="Cole o link aqui" />
+        <Input name="image" placeholder="Cole o link aqui" onChange={(event: React.ChangeEvent<HTMLInputElement>) => setImage(event.target.value)}/>
 
-        <Input name="name" placeholder="Ex: Moda Italiana" />
-        <Input name="price" placeholder="Ex: 19.90" />
+        <Input name="name" placeholder="Ex: Moda Italiana" onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)} />
+        <Input name="price" placeholder="Ex: 19.90" onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPrice(Number(event.target.value))} />
 
-        <Input name="description" placeholder="Descrição" />
+        <Input name="description" placeholder="Descrição" onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDescription(event.target.value)}/>
         <button type="submit" data-testid="add-food-button">
           <p className="text">Adicionar Prato</p>
           <div className="icon">
